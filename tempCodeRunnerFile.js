a834ee37d5c184cbfd8db18f3abb4857259643d2
tranminhphu7425 <90190034+tranminhphu7425@users.noreply.github.com>
2025-12-13 11:27:50 +0700

    .

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function wait(ms) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -11,4 +12,19 @@ wait(2000)
   .catch(err => console.error(err));
 
 
+=======
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Đợi ${ms}ms xong!`);
+    }, ms);
+  });
+}
+
+wait(2000)
+  .then(result => console.log(result))
+  .catch(err => console.error(err));
+
+
+>>>>>>> parent of 4788f88 (.)
   console.log("Hello world");
